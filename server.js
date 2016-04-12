9b5671ba42944b0a56280582cb8f75d748c12c3a
Anthony Ng <newyork.anthonyng@gmail.com>
2016-04-11 22:24:41 -0400

    Add server and Procfile for Heroku
    Add boilerplate for server with Procfile for Heroku.

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const logger = require('morgan');
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/home.html'));
+});
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  const port = server.address().port;
+
+  console.log('Server running on ' + port);
+});
