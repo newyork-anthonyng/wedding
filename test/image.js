6549cec523cecee0c91d75390867c45641d72d8f
Anthony Ng <newyork.anthonyng@gmail.com>
2016-04-20 22:13:08 -0400

    Add boilerplate mocha/chai test

---
+++ test/image.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+describe('Image API', function() {
+
+  it('should get all images');
+
+  it('should upload an image');
+});
