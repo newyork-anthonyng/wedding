588fc2f79b2a8436aa97e0917e7daa6f9ac39be8
Anthony Ng <newyork.anthonyng@gmail.com>
2016-04-26 21:22:49 -0400

    Add passing test for GET /image/sign_s3

--- routes/image.js
+++ routes/image.js
@@ -7,6 +7,7 @@ const Image = require('../models/image');
 const mongoose = require('mongoose');
 const config = require('../config');
 const utility = require('../public/utility');
+const aws = require('aws-sdk');
 
 mongoose.connect(config['mongoURI'][app.settings.env], (err, res) => {
   if(err) {
@@ -16,7 +17,12 @@ mongoose.connect(config['mongoURI'][app.settings.env], (err, res) => {
   }
 });
 
+const AWS_ACCESS_KEY = config['aws_access_key'];
+const AWS_SECRET_KEY = config['aws_secret_key'];
+const S3_BUCKET = config['s3_bucket'];
+
 router.get('/all', getAllImages);
+router.get('/sign_s3', getSignedS3Request);
 router.post('/upload', uploadImage);
 
 function getAllImages(req, res) {
@@ -38,6 +44,38 @@ function getAllImages(req, res) {
     });
   });
 };
+
+function getSignedS3Request(req, res) {
+  aws.config.update({
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY
+  });
+
+  let s3 = new aws.S3();
+  let s3_params = {
+    Bucket: S3_BUCKET,
+    Key: req.query.file_name,
+    Expires: 60,
+    ContentType: req.query.file_type,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3_params, function(err, data) {
+    if(err) {
+      res.json({
+        SUCCESS: false,
+        MESSAGE: 'Failed to retrive signed S3 request. Error: ' + err
+      });
+    } else {
+      res.json({
+        SUCCESS: true,
+        MESSAGE: 'Retrieved signed S3 request.',
+        SignedRequest: data,
+        url: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + req.query.file_name
+      });
+    };
+  });
+}
 
 function uploadImage(req, res) {
 };
--- test/image.js
+++ test/image.js
@@ -54,5 +54,24 @@ describe('Image API', function() {
       });
   });
 
+  it('should return signed S3 request on GET /image/sign_s3', function(done) {
+    chai.request(server)
+      .get('/image/sign_s3?file_name=test.pdf&file_type=application/pdf')
+      .end(function(err, res) {
+        res.should.have.a.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.a.property('SUCCESS');
+        res.body.SUCCESS.should.be.true;
+        res.body.should.have.a.property('MESSAGE');
+        res.body.MESSAGE.should.be.eq('Retrieved signed S3 request.');
+        res.body.should.have.a.property('SignedRequest');
+        res.body.should.have.a.property('url');
+        res.body.url.should.be.a('string');
+
+        done();
+      });
+  });
+
   it('should upload an image');
 });
