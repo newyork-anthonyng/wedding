565b87bfdc07b5680f3c08f904a035030545652a
Anthony Ng <newyork.anthonyng@gmail.com>
2016-04-30 21:20:22 -0400

    Add debug messages
    Add debug messages for Heroku. Getting error that "PUT" method does not exist on '/'.
    
    Add debug messages

--- public/app.js
+++ public/app.js
@@ -2,6 +2,9 @@ var fileUpload = function() {
   'use strict';
 
   function getSignedRequest(file, callback) {
+    console.log('%c Getting signed request.', 'background-color: blue; color: white;');
+    console.log('file:', file);
+
     const myUrl = '/image/sign_s3?file_name=' + file.name + '&file_type=' + file.type;
 
     $.ajax({
@@ -9,12 +12,17 @@ var fileUpload = function() {
       method: 'GET',
       dataType: 'json',
       success: function(data) {
+        console.log('data from signed request:', data);
         callback(file, data.SignedRequest, data.url);
       }
     });
   }
 
   function uploadFile(file, signedRequest, url) {
+    console.log('%c Uploading file', 'background-color: yellow;');
+    console.log('file:', file);
+    console.log('signedRequest:', signedRequest);
+    console.log('url:', url);
     $.ajax({
       url: signedRequest,
       method: 'PUT',
