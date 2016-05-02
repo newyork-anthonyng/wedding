e56d24980e8d0f56b1304ce49c7b6f9c4d285a96
Anthony Ng <newyork.anthonyng@gmail.com>
2016-05-01 20:22:21 -0400

    Add populateSlideshow()

--- public/js/imageHelper.js
+++ public/js/imageHelper.js
@@ -15,8 +15,6 @@ var imageHelper = (function() {
 		var allImages = '';
 
 		for(var i = 0; i < images.length; i++) {
-			// create image tag with src
-			console.log(images[i]['url']);
 			var currentImage = '<img src="' + images[i]['url'] + '">';
 			allImages += currentImage;
 		}
@@ -25,8 +23,27 @@ var imageHelper = (function() {
 		$album.append(allImages);
 	}
 
+	function populateSlideshow(carousel, images) {
+		var allImages = '';
+
+		for(var i = 0; i < images.length; i++) {
+			var className = 'item';
+			if(i === 0) className += ' active';
+
+			var currentSlide =
+				'<div class="' + className + '">' +
+					'<img src="' + images[i]['url'] + '">' +
+				'</div>';
+
+			allImages += currentSlide;
+		}
+
+		carousel.append(allImages);
+	}
+
 	return {
 		getAllImages: getAllImages,
-		showAllImages: showAllImages
+		showAllImages: showAllImages,
+		populateSlideshow: populateSlideshow
 	}
 })();
