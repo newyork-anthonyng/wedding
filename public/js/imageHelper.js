dd6483694ab6aef3b2ca9b803e0b8859387bfbb3
Anthony Ng <newyork.anthonyng@gmail.com>
2016-05-01 20:00:12 -0400

    Add showAllImages() to imageHelper

--- public/js/imageHelper.js
+++ public/js/imageHelper.js
@@ -6,12 +6,27 @@ var imageHelper = (function() {
 			method: 'GET',
 			dataType: 'json',
 			success: function(data) {
-				callback(data);
+				callback(data['Images']);
 			}
 		});
 	}
 
+	function showAllImages(images) {
+		var allImages = '';
+
+		for(var i = 0; i < images.length; i++) {
+			// create image tag with src
+			console.log(images[i]['url']);
+			var currentImage = '<img src="' + images[i]['url'] + '">';
+			allImages += currentImage;
+		}
+
+		var $album = $('#album');
+		$album.append(allImages);
+	}
+
 	return {
-		getAllImages: getAllImages
+		getAllImages: getAllImages,
+		showAllImages: showAllImages
 	}
 })();
